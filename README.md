3c3fb23add28bf9b81996ffe9dde723c91db8431
chillbuilds <will@chillbuilds.com>
2023-07-29 18:03:28 -0600

    added screenshot input and implemented spellcheck

--- README.md
+++ README.md
@@ -2,5 +2,14 @@
 
 reads image files from input folder and logs text from images
 
-toggle spell check to catch errors with -spell flag:
-`node e -spell`
+
+flags:
+    file:path/to/file.png - accepts local or hosted jpeg 
+    spell_check - returns 
+
+toggle spell check to catch errors with spell_check flag:
+`node e spell_check`
+<br><br>
+linux<br>
+screenshot requires imageMagick:
+`apt-get install imagemagick`
--- e.js
+++ e.js
@@ -1,12 +1,24 @@
 const fs = require('fs')
 const tesseract = require('tesseract.js')
+const screenshot = require('screenshot-desktop')
 const spelling = require('spelling')
 const dictionary = require('spelling/dictionaries/en_US.js')
 
 const dict = new spelling(dictionary)
-let logFile = './log.json'
+let logFile = './assets/log.json'
 let img = 'https://tesseract.projectnaptha.com/img/eng_bw.png'
 let lang = 'eng'
+
+let boolSpellCheck = false
+
+let logFlags = () => {
+    console.log('')
+    console.log('set image input using flags')
+    console.log('')
+    console.log('"grab" - takes a screenshot and logs the output')
+    console.log('"file:path/to/file.png" - accepts local file or url jpg/png')
+    console.log('"spell_check" - logs possible spelling errors')
+}
 
 let writeToLog = (msg) => {
     if(msg.status == 'loading tesseract core' && msg.progress == 0){
@@ -19,17 +31,18 @@ let writeToLog = (msg) => {
 }
 
 let txtProcess = (text) => {
-    console.log('\n')
-    console.log(text)
-    if(process.argv[2] == 'spell_check'){
+    if(boolSpellCheck == true){
         spellCheck(text)
+    }else{
+        console.log('\n')
+        console.log(text)
     }
 }
 
-let imgRead = () => {
+let imgRead = (input) => {
     fs.writeFileSync(logFile, '')
     tesseract.recognize(
-        img,
+        input,
         lang,
         { logger: msg => writeToLog(msg) }
         ).then(({ data: { text } }) => {
@@ -37,10 +50,56 @@ let imgRead = () => {
         })
 }
 
-// console.log(dict.lookup('penis'))
 
 let spellCheck = (text) => {
-    console.log(text)
+    console.log('\n')
+    text = text.replace(/[^\w\s]/g, ' ') // removes punctuation
+    let wordArr = text.split('\n').join(' ').split(' ') // removes new line notation
+    wordArr.forEach((word, index) => {
+        let wordCheck = dict.lookup(word)
+        if(wordCheck.found == false){
+            console.log('possible spelling error: ' + wordCheck.word + ` [${index + 1}]`)
+        }
+    })
+    console.log('\n\n' + text)
 }
 
-imgRead()
+let screenGrab = () => {
+    screenshot({format: 'png'}).then((img) => {
+        fs.writeFileSync('./assets/screenshot.png', img)
+        imgRead('./assets/screenshot.png')
+    }).catch((err) => {
+        console.log(err)
+    })
+}
+
+let flagCheck = () => {
+    let flagArr = process.argv
+    flagArr.splice(0, 2)
+    flagArr.forEach((arg, index) => {
+        switch(arg) {
+            case 'spell_check':
+              boolSpellCheck = true
+              break;
+            case '-info':
+              logFlags()
+              break;
+            case 'grab':
+                screenGrab()
+              break;
+            default:
+                if(arg.split(':')[0] == 'file'){
+                    let fileLocation = arg.replace('file:', '')
+                    imgRead(fileLocation)
+                }else{
+                    console.log(`"${arg}" is not a recognized flag`)
+                }
+          }
+    })
+    if(flagArr.length == 0){
+        console.log()
+        console.log('"node e -info" to show list of flags')
+    }
+}
+
+flagCheck()
